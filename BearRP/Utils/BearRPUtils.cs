e6b628bbc908f6e587e7f66f8f899ddf1051bd83
JiMu_Kang <dtc.initiation@gmail.com>
2026-05-22 13:13:05 +0900

    [F] Loading resources

--- BearRP/Utils/BearRPUtils.cs
+++ BearRP/Utils/BearRPUtils.cs
@@ -4,6 +4,27 @@ using UnityEngine.Rendering;
 namespace BearRP.Utils;
 
 public static class BearRPUtils {
+    public static bool GetOrLoadResource<Type>(ref Type resource, string resourcePath) where Type : Object {
+        if (resource == null) {
+            resource = Resources.Load<Type>(resourcePath);
+            return true;
+        }
+        return false;
+    }
+
+    public static bool GetOrLoadMaterial(ref Material material, string shaderPath) {
+        if (material == null) {
+            Shader shader = Shader.Find(shaderPath);
+            if (shader == null) {
+                return false;
+            }
+            material = new Material(shader);
+            return true;
+        }
+        return false;
+    }
+    
+    
     public static Mesh CreateQuad() => new Mesh() {
         vertices = [
             new Vector3(-0.5f, -0.5f, 0f),
